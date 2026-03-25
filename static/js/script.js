72be598eaa7bd97713e1c0af202298c9c7ff0927
Валентин Воробьёв <q80665646662@gmail.com>
2026-03-25 10:45:07 +0400

    Удалил ненужную функцию

--- static/js/script.js
+++ static/js/script.js
@@ -22,21 +22,6 @@ async function loadVisitors(e)
     renderVisitors(data.visitors,e)
 }
 
-function calculate_age(birth_month, birth_day, birth_year) {
-    today_date = new Date();
-    today_year = today_date.getFullYear();
-    today_month = today_date.getMonth();
-    today_day = today_date.getDate();
-    age = today_year - birth_year;
-
-    if (today_month < (birth_month - 1)) {
-        age--;
-    }
-    if (((birth_month - 1) == today_month) && (today_day < birth_day)) {
-        age--;
-    }
-    return age;
-}
 function calculate_age_str(birthDateString) {  
     // Обрабатываем аргумент birthDateString (yyyyMMdd)  
     const year = parseInt(birthDateString.substring(0, 4), 10);  
