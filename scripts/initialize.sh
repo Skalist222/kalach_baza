c07c3bfd92135fe4651faf092dfc2ee2198d6f80
Валентин Воробьёв <q80665646662@gmail.com>
2026-04-15 16:04:16 +0400

    добавленно злючее исправление

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -8,7 +8,7 @@ python3 -m venv .o
 
 echo "Устанавливаем зависимости..."
 .o/bin/python -m pip install --upgrade pip
-.o/bin/pip install -r pip.txt
+.o/bin/pip install -r scripts/pip.txt
 
 echo "Запускаем генерацию..."
 .o/bin/python generate_base.py test 1
